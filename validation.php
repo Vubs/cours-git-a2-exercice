e8fee56ea41bfbbf8024239e2954dae5c59cc123
Vincent Bertrand <b.vincent.iim@gmail.com>
2015-10-05 22:35:58 +0200

    Fix1 & Fix2

--- validation.php
+++ validation.php
@@ -7,15 +7,13 @@ if(	isset($_POST['username']) && isset($_POST['email']) && isset($_POST['passwor
 	!empty($_POST['username']) && !empty($_POST['email']) && !empty($_POST['password'])) {
 
 
-	if(isUsernameAvailable($_POST['username']) && isEmailAvailable($_POST['email'])){
-		userRegistration(PDO $db, $_POST['username'], $_POST['email'], $_POST['password']);
+	if(isUsernameAvailable($db, $_POST['username']) && isEmailAvailable($db, $_POST['email'])){
+		userRegistration($db, $_POST['username'], $_POST['email'], $_POST['password']);
 		header('Location: login.php');
 	}else{
 		header('Location: register.php');
-		$error = "l'username ou l'email est déjà pris.";
+		$_SESSION['message'] = 'Erreur: username ou email déjà pris.';
 	}
-
-
 
 }else{
 	$_SESSION['message'] = 'Erreur : Formulaire incomplet';
